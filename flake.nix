a709b82d6c47de024951bcbc8b5dcf04035ab770
Mark Bundschuh <mark@mbund.dev>
2025-09-27 17:21:32 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Lightning Rod";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "lightning-rod";
+
+          buildInputs = with pkgs; [
+            zig
+            zls
+          ];
+        };
+      }
+    );
+}
