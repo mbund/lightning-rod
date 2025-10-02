0295209ec6f05aa8ad7911cf8110dab7dc68732a
Joshua Sims <jm8hjkl@gmail.com>
2025-10-02 11:57:51 -0400

    temp

--- codegen/src/main.zig
+++ codegen/src/main.zig
@@ -48,7 +48,7 @@ const Types = struct {
 
         var it = object.iterator();
         while (it.next()) |entry| {
-            try types.put(entry.key_ptr.*, try Type.fromJson(allocator, entry.value_ptr.*));
+            try types.put(entry.key_ptr.*, try Type.fromJson(allocator, entry.key_ptr.*, entry.value_ptr.*));
         }
 
         return Types{
@@ -57,11 +57,9 @@ const Types = struct {
     }
 
     pub fn codegen(self: *const @This(), allocator: std.mem.Allocator, writer: *std.io.Writer, indent: usize) !void {
-        _ = allocator;
         var it = self.types.iterator();
         while (it.next()) |entry| {
-            try printIndent(writer, indent);
-            try writer.print("const {f} = struct {{}};\n", .{idfmt(entry.key_ptr.*)});
+            try entry.value_ptr.codegenDefinition(allocator, entry.key_ptr.*, writer, indent);
         }
     }
 };
@@ -71,22 +69,206 @@ const State = struct {
     ToClient: Types,
 };
 
+const NativeType = enum {
+    varint,
+    varlong,
+    optvarint,
+    pstring,
+    buffer,
+    u8,
+    u16,
+    u32,
+    u64,
+    i8,
+    i16,
+    i32,
+    i64,
+    bool,
+    f32,
+    f64,
+    UUID,
+    option,
+    entityMetadataLoop,
+    topBitSetTerminatedArray,
+    bitfield,
+    bitflags,
+    void,
+    restBuffer,
+    nbt,
+    optionalNbt,
+    registryEntryHolder,
+    registryEntryHolderSet,
+
+    fake,
+
+    pub fn fromString(str: []const u8) !NativeType {
+        if (std.mem.eql(u8, str, "varint")) {
+            return .varint;
+        }
+        if (std.mem.eql(u8, str, "varlong")) {
+            return .varlong;
+        }
+        if (std.mem.eql(u8, str, "optvarint")) {
+            return .optvarint;
+        }
+        if (std.mem.eql(u8, str, "pstring")) {
+            return .pstring;
+        }
+        if (std.mem.eql(u8, str, "buffer")) {
+            return .buffer;
+        }
+        if (std.mem.eql(u8, str, "u8")) {
+            return .u8;
+        }
+        if (std.mem.eql(u8, str, "u16")) {
+            return .u16;
+        }
+        if (std.mem.eql(u8, str, "u32")) {
+            return .u32;
+        }
+        if (std.mem.eql(u8, str, "u64")) {
+            return .u64;
+        }
+        if (std.mem.eql(u8, str, "i8")) {
+            return .i8;
+        }
+        if (std.mem.eql(u8, str, "i16")) {
+            return .i16;
+        }
+        if (std.mem.eql(u8, str, "i32")) {
+            return .i32;
+        }
+        if (std.mem.eql(u8, str, "i64")) {
+            return .i64;
+        }
+        if (std.mem.eql(u8, str, "bool")) {
+            return .bool;
+        }
+        if (std.mem.eql(u8, str, "f32")) {
+            return .f32;
+        }
+        if (std.mem.eql(u8, str, "f64")) {
+            return .f64;
+        }
+        if (std.mem.eql(u8, str, "UUID")) {
+            return .UUID;
+        }
+        if (std.mem.eql(u8, str, "option")) {
+            return .option;
+        }
+        if (std.mem.eql(u8, str, "entityMetadataLoop")) {
+            return .entityMetadataLoop;
+        }
+        if (std.mem.eql(u8, str, "topBitSetTerminatedArray")) {
+            return .topBitSetTerminatedArray;
+        }
+        if (std.mem.eql(u8, str, "bitfield")) {
+            return .bitfield;
+        }
+        if (std.mem.eql(u8, str, "bitflags")) {
+            return .bitflags;
+        }
+        if (std.mem.eql(u8, str, "void")) {
+            return .void;
+        }
+        if (std.mem.eql(u8, str, "restBuffer")) {
+            return .restBuffer;
+        }
+        if (std.mem.eql(u8, str, "nbt")) {
+            return .nbt;
+        }
+        if (std.mem.eql(u8, str, "optionalNbt")) {
+            return .optionalNbt;
+        }
+        if (std.mem.eql(u8, str, "registryEntryHolder")) {
+            return .registryEntryHolder;
+        }
+        if (std.mem.eql(u8, str, "registryEntryHolderSet")) {
+            return .registryEntryHolderSet;
+        }
+        if (std.mem.eql(u8, str, "switch")) {
+            return .fake;
+        }
+        if (std.mem.eql(u8, str, "container")) {
+            return .fake;
+        }
+        if (std.mem.eql(u8, str, "array")) {
+            return .fake;
+        }
+        std.debug.print("{s}\n", .{str});
+        return error.UnknownNativeType;
+    }
+
+    fn codegenType(self: NativeType) []const u8 {
+        return @tagName(self);
+    }
+};
+
+const Field = struct {
+    name: []const u8,
+    type: Type,
+};
+
 const Type = union(enum) {
     reference: []u8,
     varint,
     todo,
+    native: NativeType,
+    container: []Field,
 
-    pub fn fromJson(allocator: std.mem.Allocator, json: std.json.Value) !Type {
-        _ = allocator;
-        _ = json;
+    pub fn fromJson(allocator: std.mem.Allocator, key: []const u8, json: std.json.Value) !Type {
+        if (equalsString(json, "native")) {
+            return .{ .native = try NativeType.fromString(key) };
+        }
+        if (isConstructor(json)) |constructor| {
+            if (std.mem.eql(u8, constructor.name, "container")) {
+                const array = try expectArray(constructor.arg);
+                var fields = try allocator.alloc(Field, array.items.len);
+                for (0.., array.items) |i, field_json| {
+                    const field_object = try expectObject(field_json);
+                    const field_name = if (field_object.contains("name")) try expectString(try expectGet(field_object, "name")) else "anon";
+                    const field_type = try Type.fromJson(allocator, field_name, try expectGet(field_object, "type"));
+                    fields[i] = .{ .name = field_name, .type = field_type };
+                }
+                return .{ .container = fields };
+            }
+        }
+
         return .todo;
     }
 
-    pub fn codegen(self: *const @This(), allocator: std.mem.Allocator, writer: *std.io.Writer, indent: usize) !void {
-        _ = self;
-        _ = allocator;
-        _ = writer;
-        _ = indent;
+    pub fn codegenDefinition(self: *const @This(), allocator: std.mem.Allocator, name: []const u8, writer: *std.io.Writer, indent: usize) !void {
+        switch (self.*) {
+            .container, .todo => {
+                try printIndent(writer, indent);
+                try writer.print("const {f} = ", .{idfmt(name)});
+                try self.codegenTypeInDefinition(allocator, writer, indent);
+                try writer.print(";\n\n", .{});
+            },
+            else => {},
+        }
+    }
+
+    pub fn codegenTypeInDefinition(self: *const @This(), allocator: std.mem.Allocator, writer: *std.io.Writer, indent: usize) !void {
+        switch (self.*) {
+            .native => |native| {
+                try writer.print("{s}", .{native.codegenType()});
+            },
+            .container => |container| {
+                try writer.print("struct {{\n", .{});
+                for (container) |field| {
+                    try printIndent(writer, indent + 1);
+                    try writer.print("{f}: ", .{idfmt(field.name)});
+                    try field.type.codegenTypeInDefinition(allocator, writer, indent + 1);
+                    try writer.print(",\n", .{});
+                }
+                try printIndent(writer, indent);
+                try writer.print("}}", .{});
+            },
+            else => {
+                try writer.print("\"todo\"", .{});
+            },
+        }
     }
 };
 
@@ -116,10 +298,59 @@ fn expectObject(json: std.json.Value) !std.json.ObjectMap {
     }
 }
 
+fn expectArray(json: std.json.Value) !std.json.Array {
+    switch (json) {
+        .array => |array| {
+            return array;
+        },
+        else => {
+            return error.ExpectedArray;
+        },
+    }
+}
+
+fn expectString(json: std.json.Value) ![]const u8 {
+    switch (json) {
+        .string => |str| {
+            return str;
+        },
+        else => {
+            return error.ExpectedString;
+        },
+    }
+}
+
 fn expectGet(object: std.json.ObjectMap, key: []const u8) !std.json.Value {
     return object.get(key) orelse {
         return error.MissingKey;
     };
+}
+
+fn equalsString(json: std.json.Value, str: []const u8) bool {
+    return switch (json) {
+        .string => |s| std.mem.eql(u8, s, str),
+        else => false,
+    };
+}
+
+fn isConstructor(json: std.json.Value) ?struct { name: []const u8, arg: std.json.Value } {
+    switch (json) {
+        .array => |array| {
+            if (array.items.len != 2) {
+                return null;
+            }
+            const name = expectString(array.items[0]) catch {
+                return null;
+            };
+            return .{
+                .name = name,
+                .arg = array.items[1],
+            };
+        },
+        else => {
+            return null;
+        },
+    }
 }
 
 fn printIndent(writer: *std.io.Writer, level: usize) !void {
--- src/codegen.zig
+++ src/codegen.zig
@@ -1,56 +1,98 @@
-const optionalNbt = struct {};
-const previousMessages = struct {};
-const chunkBlockEntity = struct {};
-const entityMetadataItem = struct {};
-const entityMetadata = struct {};
-const @"switch" = struct {};
-const @"i16" = struct {};
-const vec3f = struct {};
-const @"void" = struct {};
-const array = struct {};
-const ItemSoundHolder = struct {};
-const @"u16" = struct {};
-const nbt = struct {};
-const registryEntryHolder = struct {};
-const optvarint = struct {};
-const bitflags = struct {};
-const bitfield = struct {};
-const packedChunkPos = struct {};
-const @"u8" = struct {};
-const @"i32" = struct {};
-const buffer = struct {};
-const tags = struct {};
-const varlong = struct {};
-const option = struct {};
-const soundSource = struct {};
-const vec4f = struct {};
-const @"u64" = struct {};
-const particleData = struct {};
-const minecraft_simple_recipe_format = struct {};
-const @"f32" = struct {};
-const @"u32" = struct {};
-const registryEntryHolderSet = struct {};
-const position = struct {};
-const minecraft_smelting_format = struct {};
-const chat_session = struct {};
-const ingredient = struct {};
-const @"bool" = struct {};
-const restBuffer = struct {};
-const slot = struct {};
-const string = struct {};
-const particle = struct {};
-const @"f64" = struct {};
-const varint = struct {};
-const @"i64" = struct {};
-const ByteArray = struct {};
-const entityMetadataLoop = struct {};
-const UUID = struct {};
-const @"i8" = struct {};
-const command_node = struct {};
-const ItemSoundEvent = struct {};
-const game_profile = struct {};
-const container = struct {};
-const pstring = struct {};
-const vec3f64 = struct {};
-const topBitSetTerminatedArray = struct {};
+const previousMessages = "todo";
+
+const chunkBlockEntity = struct {
+    anon: "todo",
+    y: "todo",
+    type: "todo",
+    nbtData: "todo",
+};
+
+const entityMetadataItem = "todo";
+
+const entityMetadata = "todo";
+
+const vec3f = struct {
+    x: "todo",
+    y: "todo",
+    z: "todo",
+};
+
+const ItemSoundHolder = "todo";
+
+const optvarint = "todo";
+
+const packedChunkPos = struct {
+    z: "todo",
+    x: "todo",
+};
+
+const tags = "todo";
+
+const soundSource = "todo";
+
+const vec4f = struct {
+    x: "todo",
+    y: "todo",
+    z: "todo",
+    w: "todo",
+};
+
+const particleData = "todo";
+
+const minecraft_simple_recipe_format = struct {
+    category: "todo",
+};
+
+const position = "todo";
+
+const minecraft_smelting_format = struct {
+    group: "todo",
+    category: "todo",
+    ingredient: "todo",
+    result: "todo",
+    experience: "todo",
+    cookTime: "todo",
+};
+
+const chat_session = "todo";
+
+const ingredient = "todo";
+
+const slot = struct {
+    present: "todo",
+    anon: "todo",
+};
+
+const string = "todo";
+
+const particle = struct {
+    particleId: "todo",
+    data: "todo",
+};
+
+const ByteArray = "todo";
+
+const command_node = struct {
+    flags: "todo",
+    children: "todo",
+    redirectNode: "todo",
+    extraNodeData: "todo",
+};
+
+const ItemSoundEvent = struct {
+    soundName: "todo",
+    fixedRange: "todo",
+};
+
+const game_profile = struct {
+    name: "todo",
+    properties: "todo",
+};
+
+const vec3f64 = struct {
+    x: "todo",
+    y: "todo",
+    z: "todo",
+};
+
 
